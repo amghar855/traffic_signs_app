6af89e21ac74a9d951adbee67e3384b026d55781
‪amghar abdennour‬‏ <amgharabdennour7@gmail.com>
2025-08-28 20:34:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,22 +2,22 @@ import streamlit as st
 from ultralytics import YOLO
 from PIL import Image
 
-# تحميل الموديل
+# Load the model
 model = YOLO("best.pt")
 
-st.title("Traffic Sign Detection")
+st.title("🚦 Traffic Sign Detection")
 
-# رفع صورة من المستخدم
-uploaded_file = st.file_uploader("choose image", type=["jpg", "png", "jpeg"])
+# Upload an image
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
-    # عرض الصورة
+    # Open and display the image
     image = Image.open(uploaded_file)
-    st.image(image, caption="the uploaded image ", use_column_width=True)
+    st.image(image, caption="📷 Uploaded Image", use_column_width=True)
 
-    # التنبؤ
+    # Make predictions
     results = model.predict(image)
 
-    # عرض الصورة مع البوكسات
-    res_plotted = results[0].plot()  # numpy array
-    st.image(res_plotted, caption="result", use_column_width=True)
+    # Draw bounding boxes and display results
+    res_plotted = results[0].plot()  # returns a numpy array
+    st.image(res_plotted, caption="📍 Prediction Result", use_column_width=True)
